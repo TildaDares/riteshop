c6641c4c844aed328f8837a83b0daecc470316f4
tildaudufo <mathilda.udufo@deimos.co.za>
2022-08-21 14:53:10 +0100

    nextjs setup

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Head from 'next/head'
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
+import useStyles from '../utils/styles'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
@@ -10,17 +12,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <AppBar position='static'>
+      <AppBar position='static' className={classes.navbar}>
         <Toolbar>
           <Typography>Riteshop</Typography>
         </Toolbar>
       </AppBar>
 
-      <Container>
+      <Container className={classes.main}>
         {children}
       </Container>
 
-      <footer>
+      <footer className={classes.footer}>
         <Typography>
           All rights reserved. Riteshop &copy; {new Date().getFullYear()}
         </Typography>
