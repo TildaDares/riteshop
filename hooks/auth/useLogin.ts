35c080dc94ba4ecc14086ada9ac05868e5d1cd06
tildaudufo <mathilda.udufo@deimos.co.za>
2022-08-27 21:09:54 +0100

    define hooks

---
+++ hooks/auth/useLogin.ts
@@ -0,0 +1,15 @@
+import { useCallback } from 'react';
+import { postData } from '../../utils/fetchData'
+import Cookies from 'js-cookie'
+
+const useLogin = () => {
+  return useCallback(async (email: string, password: string) => {
+    const data = await postData('users/login', {
+      email,
+      password,
+    });
+    Cookies.set('authToken', data.token, { expires: 1 });
+  }, []);
+}
+
+export default useLogin
