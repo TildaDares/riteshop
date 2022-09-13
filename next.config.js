3ee6a8e35f4a9636bddf7d9be81f437d5e24480e
tildaudufo <mathilda.udufo@deimos.co.za>
2022-09-13 15:46:02 +0100

    update next config

--- next.config.js
+++ next.config.js
@@ -5,10 +5,16 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   runtimeCaching,
+  disable: process.env.NODE_ENV === 'development',
   buildExcludes: [/middleware-manifest.json$/],
 });
 
 const nextConfig = withPWA({
-  // next config
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com'],
+  },
 });
+
 module.exports = nextConfig;
